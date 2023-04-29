24e207b47fa290acc163ebded8e9cc608af21bbb
SparkFlowGt <scinteiestefan@gmail.com>
2023-04-29 16:16:10 +0300

    2

--- src/components/featured/timeUntil.js
+++ src/components/featured/timeUntil.js
@@ -3,6 +3,13 @@ import { Slide } from "react-awesome-reveal";
 
 
 const TimeUntil = ()=>{
+    const [time,setTime]= useState({
+        days:'0',
+        hours:'0',
+        minutes:'0',
+        second:'0'
+    })
+    
     
     const renderItem =(time,value) =>(
         <div className="countdown_item">
@@ -19,20 +26,25 @@ const TimeUntil = ()=>{
     const getTimeUntil= (deadline) =>{
         const time = Date.parse(deadline) - Date.parse(new Date());
         if(time<0){
-            
+            console.log('Date passed')
         }else{
+            const seconds = Math.floor((time/1000)%60);
+            const minutes = Math.floor((time/1000/60)%60);
+            const hours = Math.floor((time/(1000*60*60))%24);
+            const days = Math.floor((time/(1000*60*60*24)));
 
+            console.log(minutes);
         }
     }
     useEffect(()=>{
-        setInterval(()=>getTimeUntil('Mar,20,2023,01:20.00'),1000) 
+        setInterval(()=>getTimeUntil(' 20 Aug 2023 01:20.00 GMT'),1000) 
     },[getTimeUntil])
     
     return(
         <Slide left delay={1000}>
            <div className="countdown_wrapper">
                 <div className="countdown_top"> 
-                Event starts in
+                Event starts in 
                 </div>
                 <div className="countdown_bottom">
   
